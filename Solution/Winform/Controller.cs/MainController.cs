daadef5b7775e626aa19f69d78fe2d76c061f61b
형성재 <tldwo125@naver.com>
2019-01-21 15:11:10 +0900

    msi파일까지 만듬

---
+++ Solution/Winform/Controller.cs/MainController.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Winform.Modules;
+using static System.Windows.Forms.ListView;
+
+namespace Winform.Controller.cs
+{
+    class MainController
+    {
+        private Commons comm;
+        private Panel head, contents, view, controller;
+        private Button btn1, btn2, btn3;
+        private ListView listView;
+        private TextBox textBox1, textBox2, textBox3, textBox4, textBox5, textBox6;
+        private Form parentForm, tagetForm;
+        private Hashtable hashtable;
+        private DB db;
+        public MainController(Form parentForm)
+        {
+            this.parentForm = parentForm;
+            comm = new Commons();
+            db = new DB();
+            getView();
+        }
+
+        private void getView()
+        {
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 1000);
+            hashtable.Add("sY", 100);
+            hashtable.Add("pX", 0);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 1);
+            hashtable.Add("name", "head");
+            head = comm.getPanel(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 1000);
+            hashtable.Add("sY", 700);
+            hashtable.Add("pX", 0);
+            hashtable.Add("pY", 100);
+            hashtable.Add("color", 4);
+            hashtable.Add("name", "contents");
+            contents = comm.getPanel(hashtable, parentForm);
+
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 1000);
+            hashtable.Add("sY", 20);
+            hashtable.Add("pX", 0);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 3);
+            hashtable.Add("name", "controller");
+            controller = comm.getPanel(hashtable, contents);
+
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 1000);
+            hashtable.Add("sY", 680);
+            hashtable.Add("pX", 0);
+            hashtable.Add("pY", 20);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "view");
+            view = comm.getPanel(hashtable, contents);
+
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 200);
+            hashtable.Add("sY", 80);
+            hashtable.Add("pX", 100);
+            hashtable.Add("pY", 10);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "btn1");
+            hashtable.Add("text", "입력");
+            hashtable.Add("click", (EventHandler)SetInsert);
+            btn1 = comm.getButton(hashtable, head);
+
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 200);
+            hashtable.Add("sY", 80);
+            hashtable.Add("pX", 400);
+            hashtable.Add("pY", 10);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "btn2");
+            hashtable.Add("text", "수정");
+            hashtable.Add("click", (EventHandler)SetUpdate);
+            btn2 = comm.getButton(hashtable, head);
+
+            hashtable = new Hashtable();
+            hashtable.Add("sX", 200);
+            hashtable.Add("sY", 80);
+            hashtable.Add("pX", 700);
+            hashtable.Add("pY", 10);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "btn3");
+            hashtable.Add("text", "삭제");
+            hashtable.Add("click", (EventHandler)SetDelete);
+            btn3 = comm.getButton(hashtable, head);
+
+            hashtable = new Hashtable();
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "listView");
+            hashtable.Add("click", (MouseEventHandler)listView_click);
+            listView = comm.getListView(hashtable, view);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 45);
+            hashtable.Add("pX", 0);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "textBox1");
+            hashtable.Add("enabled", false);
+            textBox1 = comm.getTextBox(hashtable, controller);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 100);
+            hashtable.Add("pX", 45);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "textBox2");
+            hashtable.Add("enabled", true);
+            textBox2 = comm.getTextBox(hashtable, controller);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 350);
+            hashtable.Add("pX", 145);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "textBox3");
+            hashtable.Add("enabled", true);
+            textBox3 = comm.getTextBox(hashtable, controller);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 100);
+            hashtable.Add("pX", 495);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "textBox4");
+            hashtable.Add("enabled", true);
+            textBox4 = comm.getTextBox(hashtable, controller);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 200);
+            hashtable.Add("pX", 595);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "textBox5");
+            hashtable.Add("enabled", false);
+            textBox5 = comm.getTextBox(hashtable, controller);
+
+            hashtable = new Hashtable();
+            hashtable.Add("width", 200);
+            hashtable.Add("pX", 795);
+            hashtable.Add("pY", 0);
+            hashtable.Add("color", 0);
+            hashtable.Add("name", "textBox6");
+            hashtable.Add("enabled", false);
+            textBox6 = comm.getTextBox(hashtable, controller);
+
+            GetSelect();
+        }
+
+        private void GetSelect()
+        {
+            TextClear();
+            listView.Clear();
+
+            listView.Columns.Add("nNo", 45, HorizontalAlignment.Center);        /* Notice 번호 */
+            listView.Columns.Add("nTitle", 100, HorizontalAlignment.Left);      /* Notice 제목 */
+            listView.Columns.Add("nContents", 350, HorizontalAlignment.Left);   /* Notice 내용 */
+            listView.Columns.Add("mName", 100, HorizontalAlignment.Center);     /* Notice 작성자 이름 */
+            listView.Columns.Add("regDate", 200, HorizontalAlignment.Left);     /* Notice 작성 현재날짜 */
+            listView.Columns.Add("modDate", 200, HorizontalAlignment.Left);     /* Notice 수정 현재날짜 */
+
+            // 보여 주기 가상 데이터 -> WebAPI를 이용하여 데이터 가져올것!
+            //listView.Items.Add(new ListViewItem(new string[] { "3", "제목3", "내용3", "Winform", "2018-12-07", "2016-12-07" }));
+            //listView.Items.Add(new ListViewItem(new string[] { "2", "제목2", "내용2", "스마트", "2018-12-06", "2016-12-07" }));
+            //listView.Items.Add(new ListViewItem(new string[] { "1", "제목1", "내용1", "관리자", "2018-12-05", "2016-12-07" }));
+            listView.Items.AddRange(db.Select());
+        }
+
+        private void SetInsert(object o, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                MessageBox.Show("리스트를 선택해서 입력할 수 없습니다.");
+                return;
+            }
+            if (!db.Insert(textBox2.Text, textBox3.Text, textBox4.Text))
+            {
+                MessageBox.Show("Insert Error...Member에 있는 이름을 입력해주세요");
+            }
+            GetSelect();
+        }
+        private void SetUpdate(object o, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("수정할 리스트를 선택해주세요");
+                return;
+            }
+            if (!db.Update(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text))
+            {
+                MessageBox.Show("Update Error...Member에 있는 이름을 입력해주세요");
+            }
+            GetSelect();
+        }
+        private void SetDelete(object o, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("삭제할 리스트를 선택해주세요");
+                return;
+            }
+            if (!db.Delete(textBox1.Text, textBox4.Text))
+            {
+                MessageBox.Show("Delete Error...Member에 있는 이름을 입력해주세요");
+            }
+            GetSelect();
+        }
+        private void listView_click(object o, EventArgs a)
+        {
+            //MessageBox.Show("listView_click");
+            ListView lv = (ListView)o;
+            SelectedListViewItemCollection list = lv.SelectedItems;
+            ListViewItem lvi = list[0];
+
+            textBox1.Text = lvi.SubItems[0].Text;
+            textBox2.Text = lvi.SubItems[1].Text;
+            textBox3.Text = lvi.SubItems[2].Text;
+            textBox4.Text = lvi.SubItems[3].Text;
+            textBox5.Text = lvi.SubItems[4].Text;
+            textBox6.Text = lvi.SubItems[5].Text;
+        }
+        private void TextClear()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
+    }
+}
---
+++ Solution/Winform/Modules/DB.cs
@@ -0,0 +1,146 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform.Modules
+{
+    public class DB
+    {
+        private WebClient client;
+        private NameValueCollection collection;
+
+        public DB()
+        {
+            client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            client.Encoding = Encoding.UTF8;
+
+            string path = "/public/IPinfo.json";
+            string result = new StreamReader(File.OpenRead(path)).ReadToEnd();
+            JObject jo = JsonConvert.DeserializeObject<JObject>(result);
+            Hashtable map = new Hashtable();
+            foreach (JProperty jp in jo.Properties())
+            {
+                map.Add(jp.Name, jp.Value);
+            }
+            Program.url = string.Format("http://{0}/", map["server"].ToString());
+            Console.WriteLine("DB접속정보 ==> " + Program.url);
+        }
+        public ListViewItem[] Select()
+        {
+            ListViewItem[] result;//검색해온 리스트를 담아둘 곳
+
+            string url = Program.url + "select";
+            Stream stream = client.OpenRead(url);
+            StreamReader sr = new StreamReader(stream);
+            string str = sr.ReadToEnd();
+            ArrayList obj = JsonConvert.DeserializeObject<ArrayList>(str);
+            result = new ListViewItem[obj.Count];
+            for (int i = 0; i < obj.Count; i++)
+            {
+
+                JObject jo = (JObject)obj[i];
+                string[] arr = new string[jo.Count];
+
+                foreach (JProperty jp in jo.Properties())
+                {
+                    switch (jp.Name)
+                    {
+                        case "nNo":
+                            arr[0] = jp.Value.ToString();
+                            break;
+                        case "nTitle":
+                            arr[1] = jp.Value.ToString();
+                            break;
+                        case "nContents":
+                            arr[2] = jp.Value.ToString();
+                            break;
+                        case "mName":
+                            arr[3] = jp.Value.ToString();
+                            break;
+                        case "regDate":
+                            arr[4] = jp.Value.ToString();
+                            break;
+                        case "modDate":
+                            arr[5] = jp.Value.ToString();
+                            break;
+                    }
+                }
+                result[i] = new ListViewItem(arr);
+            }
+            return result;
+        }
+        public bool Insert(string nTitle, string nContents, string mName)
+        {
+            //==============입력받은 값에 대해서 추가해주는 부분=====================================
+            collection = new NameValueCollection();
+            collection.Add("nTitle", nTitle);
+            collection.Add("nContents", nContents);
+            collection.Add("mName", mName);
+
+            string url = Program.url + "insert";
+            string method = "POST";
+            byte[] result = client.UploadValues(url, method, collection);
+            string strResult = Encoding.UTF8.GetString(result);
+            if (strResult == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Update(string nNo, string nTitle, string nContents, string mName)
+        {
+            //==============입력받은 값에 대해서 수정해주고 수정날짜를 바꿔주는 부분======================================
+            collection = new NameValueCollection();
+            collection.Add("nNo", nNo);
+            collection.Add("nTitle", nTitle);
+            collection.Add("nContents", nContents);
+            collection.Add("mName", mName);
+
+            string url = Program.url + "update";
+            string method = "POST";
+            byte[] result = client.UploadValues(url, method, collection);
+            string strResult = Encoding.UTF8.GetString(result);
+            if (strResult == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Delete(string nNo, string mName)
+        {
+            //==============입력받은 값에 대해서 delYn을 'Y'로 고쳐주고 수정날짜를 바꿔주는 부분=========================
+            collection = new NameValueCollection();
+            collection.Add("nNo", nNo);
+            collection.Add("mName", mName);
+
+            string url = Program.url + "delete";
+            string method = "POST";
+            byte[] result = client.UploadValues(url, method, collection);
+            string strResult = Encoding.UTF8.GetString(result);
+            if (strResult == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
